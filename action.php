77ff36057fb682b40cffc52600c409cf08e85d2c
backpackerpanda2015 <backpackerpanda2015@gmail.com>
2017-01-20 09:05:34 +0530

    updated code

--- action.php
+++ action.php
@@ -1,15 +1,12 @@
-header('Content-Type: application/excel');
-header('Content-Disposition: attachment; filename="sample.csv"');
+
 
 <?php
-$firstarray = array(1 => 'Site Visit', 2 => 'Dealer/sub-dealer Visit', 3 => 'Influencer Visit', 4 => 'Technical Meet', 5 => 'Lead Generated');
 echo "Thanks for submiting the data<pre>";
-$file = fopen("vinitng.csv","a");
+
+$firstarray = array(1 => 'Site Visit', 2 => 'Dealer/sub-dealer Visit', 3 => 'Influencer Visit', 4 => 'Technical Meet', 5 => 'Lead Generated');
 $_POST['first'] = $firstarray[$_POST['first']];
-echo '<pre>';print_r($_POST);
-$data = implode(",",$_POST);
-echo $data;//exit;
+$_POST['datetime'] = date('d-m-Y H:i:s');
+$file = fopen("vinitng.csv","a");
 fputcsv($file,$_POST);
 fclose($file);
-// print_r($_POST);
 ?>
